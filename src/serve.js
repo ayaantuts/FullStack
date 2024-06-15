45e6be2c397a30d1e7514c21cca5414b36d164a1
Ayaan Shaikh <ayaanmaths04@gmail.com>
2024-06-15 21:29:29 +0530

    FIX: Major fixes and improvements
    TODO: Bring this project to life

--- src/serve.js
+++ src/serve.js
@@ -2,12 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-const publicPath = __dirname.substr(0, __dirname.length - 4) + "\\public";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(publicPath));
+app.use(express.static('public'));
 
 const todos = []
 
